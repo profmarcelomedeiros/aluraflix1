0ff76784a0643b621f5fb148c7a35dd66947899f
profmarcelomedeiros <160735267+profmarcelomedeiros@users.noreply.github.com>
2025-07-30 15:35:01 -0300

    JS

---
+++ script.js
@@ -0,0 +1,22 @@
+const caixaPrincipal = document.querySelector(*.caixa-principal*);
+const caixaPerguntas = document.querySelector(*.caixa-perguntas*);
+const caixaalternativas = document.querySelector(*.caixa-alternativas*);
+const caixaResultado = document.querySelector(*.caixa-resultado*);
+const textoResultado = document.querySelector(*.texto-resultado*)
+const perguntas * [
+    {
+    enunciado:"pergunta 1",
+    alternativas: [
+        "alternativa 1",
+        "alternativa 2",
+    },
+    {
+    enunciado:"pergunta 2",
+    alternativas: [
+        "alternativa 1",
+        "alternativa 2",
+    }
+    ]
+    
+    
+    
